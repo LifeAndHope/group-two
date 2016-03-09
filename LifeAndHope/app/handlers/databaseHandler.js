c2b35c7f7a7ea4512d923f4c47c139b10a46592b
Kristian <kristian.aabrekk@gmail.com>
2016-03-09 14:07:05 +0100

    Methods to get notes

--- LifeAndHope/app/handlers/databaseHandler.js
+++ LifeAndHope/app/handlers/databaseHandler.js
@@ -7,6 +7,10 @@ var DatabaseHandler = (function () {
             baseURL: 'https://api.securedb.co:443/securedbapi/data/P88cSuYR/default',
             headers: { "Authorization": "Basic TzFLT0JPS0pWTDpaT0VJUTdIUTExTjUyNDU=" } //TODO: change to JWT?
         });
+        this.fileConnection = axios.create({
+            baseURL: 'https://api.securedb.co:443/securedbapi/files/P88cSuYR/default',
+            headers: { "Authorization": "Basic TzFLT0JPS0pWTDpaT0VJUTdIUTExTjUyNDU=" }
+        });
     }
     DatabaseHandler.prototype.login = function (username, password) {
         return this.accountConnection.post("/authenticate", {
@@ -29,6 +33,13 @@ var DatabaseHandler = (function () {
     };
     DatabaseHandler.prototype.getSponsor = function () {
         return this.dataConnection.get('/sponsor?fields=*');
+    };
+    DatabaseHandler.prototype.getNote = function (tableName, instanceId) {
+        var requestUrl = '/note?fields=*&filters=table_name%3D\'' + tableName + '\'%20AND%20instance_id%3D\'' + instanceId + '\'&&&';
+        return this.dataConnection.get(requestUrl);
+    };
+    DatabaseHandler.prototype.getNotesFromChild = function (childId) {
+        return this.getNote('child', childId);
     };
     return DatabaseHandler;
 })();
--- LifeAndHope/app/handlers/databaseHandler.ts
+++ LifeAndHope/app/handlers/databaseHandler.ts
@@ -8,17 +8,23 @@ export class DatabaseHandler {
 
     accountConnection: any;
     dataConnection : any;
+    fileConnection : any;
 
 
     constructor() {
         this.accountConnection = axios.create({
             baseURL: 'https://api.securedb.co:443/securedbapi/account/P88cSuYR/default',
-            //headers: {"Authorization": "Basic TzFLT0JPS0pWTDpaT0VJUTdIUTExTjUyNDU="}
+                //headers: {"Authorization": "Basic TzFLT0JPS0pWTDpaT0VJUTdIUTExTjUyNDU="}
         });
 
         this.dataConnection = axios.create({
             baseURL: 'https://api.securedb.co:443/securedbapi/data/P88cSuYR/default',
             headers: {"Authorization": "Basic TzFLT0JPS0pWTDpaT0VJUTdIUTExTjUyNDU="} //TODO: change to JWT?
+        });
+
+        this.fileConnection = axios.create({
+            baseURL: 'https://api.securedb.co:443/securedbapi/files/P88cSuYR/default',
+            headers: {"Authorization": "Basic TzFLT0JPS0pWTDpaT0VJUTdIUTExTjUyNDU="}
         });
     }
 
@@ -46,4 +52,15 @@ export class DatabaseHandler {
     getSponsor() : any{
         return this.dataConnection.get('/sponsor?fields=*')
     }
+
+    getNote(tableName : string, instanceId : string) : any{
+        let requestUrl = '/note?fields=*&filters=table_name%3D\''+ tableName +'\'%20AND%20instance_id%3D\''+ instanceId +'\'&&&'
+
+        return this.dataConnection.get(requestUrl)
+    }
+
+    getNotesFromChild(childId : string){
+        return this.getNote('child', childId);
+    }
+
 }
