3c44565f545b3dc01b68a74c9ada2464d080716f
agunde email = and.gunde@gmail.com <and.gunde@gmail.com>
2016-03-09 14:10:14 +0100

    login så og si up to date

--- LifeAndHope/app/components/login.ts
+++ LifeAndHope/app/components/login.ts
@@ -1,5 +1,7 @@
 import {Component} from 'angular2/core';
 import {CanActivate} from 'angular2/router';
+import {FormBuilder, Validators} from 'angular2/common';
+import {UserService} from "../services/userService";
 
 @Component({
     selector: 'login',
@@ -8,11 +10,27 @@ import {CanActivate} from 'angular2/router';
 })
 
 export class LoginComponent {
-    // Do fancy stuff
-    loginFunction(){
-        //new DatabaseService().login(username,password)
+    userForm: any;
 
+    constructor(private _formBuilder: FormBuilder) {
+        this.userForm = this._formBuilder.group({
+            'username': ['', Validators.required],
+            'password': ['', Validators.required]
+        });
     }
 
-
+    loginUser() {
+        if (this.userForm.dirty && this.userForm.valid) {
+            alert(`username: ${this.userForm.value.username} password: ${this.userForm.value.password}`);
+            UserService.login(this.userForm.value.username, this.userForm.value.password)
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(response => {
+                    console.log(response);
+                })
+        }
+    }
 }
+
+LoginComponent.parameters = [FormBuilder];
